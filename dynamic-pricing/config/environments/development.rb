840cf3d39d91d83b6d697363033184bda928bcb9
Ajeng Sugiarti <ajeng.sugiarti@gmail.com>
2026-03-26 18:06:04 +0700

    Add Redis dependency

--- dynamic-pricing/config/environments/development.rb
+++ dynamic-pricing/config/environments/development.rb
@@ -17,19 +17,13 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
-  # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
-
+  config.cache_store = :redis_cache_store, {
+    url: ENV.fetch("REDIS_URL"),
+    namespace: "tripla-pricing-cache",
+    expires_in: 5.minutes
+  }
+  config.action_controller.perform_caching = true
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -40,13 +34,13 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation_warnings = []
 
   # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
+  # config.active_record.migration_error = :page_load
 
   # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
+  # config.active_record.verbose_query_logs = true
 
   # Highlight code that enqueued background job in logs.
-  config.active_job.verbose_enqueue_logs = true
+  # config.active_job.verbose_enqueue_logs = true
 
 
   # Raises error for missing translations.
